510c93a6c085f7ce7412bd917c6e85bfc65585bd
Scottacious <scottgeilen99@gmail.com>
2019-08-11 12:49:07 -0400

    Added 3 more empty pages. Added the theme of the website; ARIZONA. Made the front page a little more interesting

--- index.php
+++ index.php
@@ -14,7 +14,15 @@
 		<div class="header">
 			<h2>Home</h2>
 		</div>
-
+		<div class="navbar">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="why.php">Why Arizona?</a></li>
+				<li><a href="how.php">How Arizona?</a></li>
+				<li><a href="where.php">Where Arizona?</a></li>
+				<li><a href="where.php">Where Arizona?</a></li><li><a href="index.php?logout='1'" style="color: red;">Logout</a></li>
+			</ul>
+		</div>
 		<div class="content">
 			<?php if (isset($_SESSION['success'])): ?>
 				<div class="error success">
@@ -26,10 +34,20 @@
 					</h3>
 				</div>
 			<?php endif ?>
-			<?php if (isset($_SESSION["username"])): ?>
-				<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-				<p><a href="index.php?logout='1'" style="color: red;">Logout</a></p>
-			<?php endif ?>
+			<div class="content2">
+				<?php if (isset($_SESSION["username"])): ?>
+					<p align="center">Welcome <strong><?php echo $_SESSION['username']; ?>!</strong></p>
+				<?php endif ?>
+				<h2 align="center">Want to know why I want to move to Arizona? You came to the right place!</h2>
+				<h3 align="center">Why did you have to register and login? Good question!</h3>
+				<p align="center">So I could learn how to do something like that. That's literally it. Now, let's learn about Arizona!</p>
+				<div>
+					<a href="https://ibb.co/hyy9VSD"><img src="https://i.ibb.co/jTTfDSg/grandcanyon.jpg" alt="grandcanyon" border="0" align="center"></a>
+					<p>Nearly two billion years of Earth's geological history have been exposed as the Colorado River and its tributaries cut their channels through layer after layer of rock while the Colorado Plateau was uplifted. While some aspects about the history of incision of the canyon are debated by geologists several recent studies support the hypothesis that the Colorado River established its course through the area about 5 to 6 million years ago. Since that time, the Colorado River has driven the down-cutting of the tributaries and retreat of the cliffs, simultaneously deepening and widening the canyon.
+
+					<br><br>For thousands of years, the area has been continuously inhabited by Native Americans, who built settlements within the canyon and its many caves. The Pueblo people considered the Grand Canyon a holy site, and made pilgrimages to it. The first European known to have viewed the Grand Canyon was García López de Cárdenas from Spain, who arrived in 1540.</p>
+				</div>
+			</div>
 		</div> 
 	</body>
 </html>
--- register.php
+++ register.php
@@ -9,32 +9,29 @@
 		<div class="header">
 			<h2>Register</h2>
 		</div>
-
-		<form method="post" action="register.php">
-			<!-- display validation errors -->
-			<?php include('errors.php'); ?>
-			<div class="input-group">
-				<label>Username</label>
-				<input type="text" name="username" value="<?php echo $username; ?>">
-			</div>
-			<div class="input-group">
-				<label>Email</label>
-				<input type="text" name="email" value="<?php echo $email; ?>">
-			</div>
-			<div class="input-group">
-				<label>Password</label>
-				<input type="password" name="password01">
-			</div>
-			<div class="input-group">
-				<label>Confirm password</label>
-				<input type="password" name="password02">
-			</div>
-			<div class="input-group">
-				<button type="submit" name="register" class="btn">Register</button>
-			</div>
-			<p>
-				Already a member? <a href="login.php">Sign in</a>
-			</p>
-		</form>
+		<div class="content">
+			<form method="post" action="register.php">
+				<!-- display validation errors -->
+				<?php include('errors.php'); ?>
+				<div class="input-group">
+					<input type="text" name="username" value="<?php echo $username; ?>" placeholder="Username" >
+				</div>
+				<div class="input-group">
+					<input type="text" name="email" value="<?php echo $email; ?>" placeholder="example@example.com">
+				</div>
+				<div class="input-group">
+					<input type="password" name="password01" placeholder="Password" >
+				</div>
+				<div class="input-group">
+					<input type="password" name="password02" placeholder="Confirm password" >
+				</div>
+				<div class="input-group">
+					<button type="submit" name="register" class="btn">Register</button>
+				</div>
+				<p>
+					Already a member? <a href="login.php">Sign in</a>
+				</p>
+			</form>
+		</div>
 	</body>
 </html>
